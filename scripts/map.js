5d17f0b6581549670698f5f7d668e5c14f3c9880
Diana Gorchichnaya <dianagorchichnaya@gmail.com>
2021-01-30 06:05:09 +0400

    Добавлена Яндекс-карта

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,33 @@
-// ymaps.ready(init);
+let myMap;
 
-// function init() {
-//     var myMap = new ymaps.Map("map", {
-//         center: [55.76, 37.64],
-//         zoom: 7
-//     });
-// }
+const init = () => {
+    myMap = new ymaps.Map("map", {
+        center: [56.866582, 53.209872],
+        zoom: 13,
+        controls: [],
+    });
+
+    let coords = [
+            [56.859484, 53.176862],
+            [56.846717, 53.197888],
+            [56.887275, 53.249778],
+            [56.868083, 53.222445],
+        ],
+        myCollection = new ymaps.GeoObjectCollection({}, {
+            draggable: false,
+            iconLayout: 'default#image',
+            iconImageHref: '/2020-11-23-web-chocco/images/svg/marker.svg',
+            iconImageSize: [58, 73],
+            iconImageOffset: [-29, -73]
+        });
+
+    for (let i = 0; i < coords.length; i++) {
+        myCollection.add(new ymaps.Placemark(coords[i]));
+    }
+
+    myMap.geoObjects.add(myCollection);
+
+    myMap.behaviors.disable('scrollZoom');
+};
+
+ymaps.ready(init);
